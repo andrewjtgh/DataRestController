41639cd5063173f01c89631f8bf08cca4b4d2391
darryl1975 <darryl1975@hotmail.com>
2022-09-15 13:15:11 +0800

    first commit

---
+++ src/main/java/sg/nus/edu/iss/springbootcd/cicd/demo/CicdDemoApplication.java
@@ -0,0 +1,13 @@
+package sg.nus.edu.iss.springbootcd.cicd.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdDemoApplication.class, args);
+	}
+
+}
